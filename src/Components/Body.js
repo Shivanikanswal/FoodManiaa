2faa6dc78cd26d873497e92815d8cf0c5828f43e
Shivani Kanswal <shivanikanswal@gmail.com>
2023-09-19 12:27:34 +0530

    CORS Error Resolve

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -21,7 +21,7 @@ const Body = () => {
 
   const fetchData = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9121181&lng=77.6445548&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9121181&lng=77.6445548&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
 
     const jsonData = await data.json();
